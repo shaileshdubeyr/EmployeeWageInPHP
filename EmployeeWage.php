b3557c5af4f19b9918179ff94f59dba78c4f3fe6
SHAILESH <shaileshdubeyr@gmail.com>
2021-12-23 10:40:30 +0530

    Added : welcome meaaasge

---
+++ EmployeeWage.php
@@ -0,0 +1,32 @@
+<?php
+class EmployeeWages{
+    private $wagePerHour = 20;
+    private $fullDayHour = 8;
+    public function __construct(){
+        echo "welcome TO Employee Wage";
+    }
+
+    function UcOne(){
+        $this->randomValue = rand(0,1);
+        if($this->randomValue == 1){
+            echo "Employee is present\n";
+            return $this->randomValue;
+        }else{
+            echo "Employee is absent\n";
+            return $this->randomValue;
+        }
+    }
+
+    function UcTwo(){
+        $oneDayWage = 0;
+        $employeeIsPresent = $this->UcOne();
+        if($employeeIsPresent == 1){
+            $oneDayWage = $this->wagePerHour * $this->fullDayHour;
+            echo "Total Wage of the Day is :- ".$oneDayWage; 
+        }else{
+            echo "Total Wage of the day is :- $oneDayWage";
+        }
+    }
+}
+$employeewage = new EmployeeWages();
+?>
