7529c6e395252ba15d6279bc2368536354e0a247
SHAILESH <shaileshdubeyr@gmail.com>
2021-12-23 11:00:40 +0530

    Added : code for eployee present for half day also

--- EmployeeWage.php
+++ EmployeeWage.php
@@ -2,11 +2,16 @@
 class EmployeeWages{
     private $wagePerHour = 20;
     private $fullDayHour = 8;
+    private $halfDayHour = 5;
 
     function UcOne(){
-        $this->randomValue = rand(0,1);
+        $this->randomValue = rand(0,2);
         if($this->randomValue == 1){
             echo "Employee is present\n";
+            return $this->randomValue;
+        }
+        elseif($this->randomValue == 2){
+            echo "Employee is present for half day\n";
             return $this->randomValue;
         }else{
             echo "Employee is absent\n";
@@ -24,7 +29,20 @@ class EmployeeWages{
             echo "Total Wage of the day is :- $oneDayWage";
         }
     }
+   public function UcThree(){
+        $oneDayWage = 0;
+        $employeeIsPresent = $this->UcOne();
+        if($employeeIsPresent == 1){
+            $oneDayWage = $this->wagePerHour * $this->fullDayHour;
+            echo "Total Wage of the Day is :- ".$oneDayWage; 
+        }elseif($employeeIsPresent == 2){
+            $oneDayWage = $this->wagePerHour * $this->halfDayHour;
+            echo "Total Wage of the Day is :- ".$oneDayWage;
+        }else{
+            echo "Total Wage of the day is :- $oneDayWage";
+        }
+    }
 }
 $employeewage = new EmployeeWages();
-$employeewage->UcTwo();
+$employeewage->UcThree();
 ?>
