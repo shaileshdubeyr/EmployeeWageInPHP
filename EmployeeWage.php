1c64f80f5e5190e8d2d2eb669a02937a14a752d6
SHAILESH <shaileshdubeyr@gmail.com>
2021-12-23 11:28:17 +0530

    Added : code calculatig employee wages for month

--- EmployeeWage.php
+++ EmployeeWage.php
@@ -3,11 +3,13 @@ class EmployeeWages{
     private $wagePerHour = 20;
     private $fullDayHour = 8;
     private $halfDayHour = 5;
+    private $wagesformonth = 20;
 
+    //checking employee is present or not
     function UcOne(){
         $this->randomValue = rand(0,2);
         if($this->randomValue == 1){
-            echo "Employee is present\n";
+            echo "Employee is present for full day\n";
             return $this->randomValue;
         }
         elseif($this->randomValue == 2){
@@ -19,9 +21,11 @@ class EmployeeWages{
         }
     }
 
+    //checking employee status and calculating wages
     function UcTwo(){
         $oneDayWage = 0;
-        $employeeIsPresent = $this->UcOne();
+        //calling method to check employee status
+        $employeeIsPresent = $this->UcOne(); 
         if($employeeIsPresent == 1){
             $oneDayWage = $this->wagePerHour * $this->fullDayHour;
             echo "Total Wage of the Day is :- ".$oneDayWage; 
@@ -29,23 +33,27 @@ class EmployeeWages{
             echo "Total Wage of the day is :- $oneDayWage";
         }
     }
-   public function UcFour(){
+
+    //checking employee status and calculating employee wages for month
+    function UcFive(){
         $oneDayWage = 0;
+        //calling method to check employee status
         $employeeIs = $this->UcOne();
         switch($employeeIs){
             case 1:
-                $oneDayWage = $this->wagePerHour * $this->fullDayHour;
-                echo "Total Wage of the Day is :- ".$oneDayWage;
+                //calculating wages
+                $oneDayWage = $this->wagePerHour * $this->fullDayHour * $this->wagesformonth;
+                echo "Total Wage for the month is :- ".$oneDayWage;
                 break; 
             case 2:
-                $oneDayWage = $this->wagePerHour * $this->halfDayHour;
-                echo "Total Wage of the Day is :- ".$oneDayWage;
+                $oneDayWage = $this->wagePerHour * $this->halfDayHour * $this->wagesformonth;
+                echo "Total Wage for the month is :- ".$oneDayWage;
                 break;
             default:
-                echo "Total Wage of the day is :- $oneDayWage";
+                echo "Total Wage for the month is :- $oneDayWage";
         }
     }
 }
 $employeewage = new EmployeeWages();
-$employeewage->UcFour();
+$employeewage->UcFive();
 ?>
