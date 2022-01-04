efe78f6ecd9044e2a3146479a2b4716c658c151a
SHAILESH <shaileshdubeyr@gmail.com>
2022-01-04 06:44:41 +0530

    Added : code inserting multiple object for a company in array

--- EmployeeWage.php
+++ EmployeeWage.php
@@ -14,12 +14,13 @@ class EmployeeWages
      * no return type
      * checking employee wage for month day
      */
-    function __construct($empWagePerHour, $workDaysPerMonth, $workingHour, $company)
+    function __construct($company,$empWagePerHour, $workDaysPerMonth, $workingHour)
     {
         $this->empWagePerHour = $empWagePerHour;
         $this->workDaysPerMonth = $workDaysPerMonth;
         $this->workingHour = $workingHour;
         $this->company = $company;
+        $this->employeeWagesForTheMonth();
     }
     private function employeeWagesForTheMonth()
     {
@@ -33,7 +34,7 @@ class EmployeeWages
             $workDayPerMonth++;
             switch ($randomValue) {
                 case 1:
-                    echo "Employee is present for full day\n";
+                    echo "Employee is present for full Day\n";
                     $this->workingHours = 8;
                     break;
                 case 2:
@@ -45,17 +46,28 @@ class EmployeeWages
         }
         //calculating total wage of employee
         $totalWageIs = $this->empWagePerHour * $hour * $workDayPerMonth;
-        echo "total wage of $this->company for one month is $totalWageIs";
-    }
-    function __destruct()
-    {
-        $this->employeeWagesForTheMonth();
+        echo "total wage of $this->company for one month is $totalWageIs\n";
     }
 }
 /**
  * creating object of the class
  */
-$dmart = new EmployeeWages(readline("enter the wage per hour :- "),
-                           readline("working day per month :- "),
-                           readline("Enter total Working hours :- "),
-                           "Dmart");
+$employeeWageBuilder = [];
+$loop = true;
+while($loop){
+    $loop = readline("Do you want to add the company wages then type 'y' : - ");
+    if($loop == 'y'){
+        $companyObj = new EmployeeWages(readline("enter the  Company Name :- "),
+                                        readline("enter the wage per hour :- "),
+                                        readline("working day per month :- "),
+                                        readline("Enter total Working hours :- "));
+        $companykey = readline("Ente the key"); 
+        $employeeWageBuilder[$companykey] = $companyObj;
+    }else {
+        $loop = false; 
+    } 
+}
+print_r($employeeWageBuilder);
+
+
+    
